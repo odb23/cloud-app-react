ee55f32592a0036d1119d05c97013b6442ebf38b
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-30 00:55:06 +0100

    added hero section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <div className='w-screen h-[50px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
-            <div className='px-2 flex justify-between items-center w-full h-full'>
+            <div className='px-4 flex justify-between items-center w-full h-full'>
                 <div className='flex item-center'>
                     <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>ODB</h1>
                     <ul className='hidden md:flex'>
