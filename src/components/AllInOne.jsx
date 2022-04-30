8af2fbb258e27cfbd41bbe8143fbf2b8bbc0cae3
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-30 02:54:39 +0100

    added all-in-one section

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,106 @@
+import { CheckIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+const AllInOne = () => {
+    return (
+        <div className='w-full my-32'>
+            <div className='max-w-[1240px] mx-auto px-2'>
+                <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+                <p className='text-2xl py-8 text-gray-500 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! Asperiores minima deserunt, aliquam velit, quasi culpa consequuntur impedit tempore dolore dignissimos magnam expedita nam, repudiandae quaerat eligendi voluptatum facilis?</p>
+
+                <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 p-6 pt-4'>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                     <div className='flex'>
+                        <div>
+                            <CheckIcon className='w-7 mr-4 text-green-600' />
+                        </div>
+                        <div>
+                            <h3 className='font-bold text-lg'>Notifications</h3>
+                            <p className='text-lg pt-2 pb-4'>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum rerum hic libero, alias delectus atque minus voluptates. Tempora consequuntur ipsam vero nemo numquam rem iusto, illo eius nihil non possimus!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AllInOne
